56e30f0492379973745da9623e0aa691556dccd2
rohjimin0 <rohjimin1@naver.com>
2026-04-20 00:43:30 +0900

    next.config.js 수정 - serverExternalPackages 경고 해결

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['sql.js'],
+  experimental: {
+    serverComponentsExternalPackages: ["sql.js"],
+  },
   webpack: (config) => {
-    config.externals = [...(config.externals || []), { 'sql.js': 'sql.js' }]
+    config.externals = [...(config.externals || []), { "sql.js": "sql.js" }]
     return config
   },
 }
-
 module.exports = nextConfig
